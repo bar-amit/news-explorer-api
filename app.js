6be646a294484770618f9ffd19d7ace3ca899317
bar-amit <bar.amit@outlook.com>
2022-01-03 10:47:11 +0200

    create express

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log('Listening on port 3000'));
