913dd5608079720eed0046e55299206d8bd1a6c9
bar-amit <bar.amit@outlook.com>
2022-01-03 11:50:15 +0200

    create mogoose document models

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -7,4 +8,5 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb://localhost:27017/news-explorer-db');
 app.listen(3000, () => console.log('Listening on port 3000'));
