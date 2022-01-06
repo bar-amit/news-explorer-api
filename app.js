6c468e1186d20dac5a5836b2e2bb02d5ad2b22f9
bar-amit <bar.amit@outlook.com>
2022-01-06 21:46:26 +0200

    fix: remove console.log

--- app.js
+++ app.js
@@ -44,4 +44,4 @@ app.use(errors());
 app.use(errorHandler);
 
 mongoose.connect('mongodb://localhost:27017/news-explorer-db');
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(3000);
